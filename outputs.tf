c02db88222fafeb16df4d50ae96933a77600e5bf
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,57 @@
-output "express_route_ports" {
-  description = "All express_route_port resources"
-  value       = azurerm_express_route_port.express_route_ports
-}
 output "express_route_ports_bandwidth_in_gbps" {
-  description = "List of bandwidth_in_gbps values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.bandwidth_in_gbps]
+  description = "Map of bandwidth_in_gbps values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.bandwidth_in_gbps }
 }
 output "express_route_ports_billing_type" {
-  description = "List of billing_type values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.billing_type]
+  description = "Map of billing_type values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.billing_type }
 }
 output "express_route_ports_encapsulation" {
-  description = "List of encapsulation values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.encapsulation]
+  description = "Map of encapsulation values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.encapsulation }
 }
 output "express_route_ports_ethertype" {
-  description = "List of ethertype values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.ethertype]
+  description = "Map of ethertype values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.ethertype }
 }
 output "express_route_ports_guid" {
-  description = "List of guid values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.guid]
+  description = "Map of guid values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.guid }
 }
 output "express_route_ports_identity" {
-  description = "List of identity values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.identity]
+  description = "Map of identity values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.identity }
 }
 output "express_route_ports_link1" {
-  description = "List of link1 values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.link1]
+  description = "Map of link1 values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.link1 }
 }
 output "express_route_ports_link2" {
-  description = "List of link2 values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.link2]
+  description = "Map of link2 values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.link2 }
 }
 output "express_route_ports_location" {
-  description = "List of location values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.location]
+  description = "Map of location values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.location }
 }
 output "express_route_ports_mtu" {
-  description = "List of mtu values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.mtu]
+  description = "Map of mtu values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.mtu }
 }
 output "express_route_ports_name" {
-  description = "List of name values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.name]
+  description = "Map of name values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.name }
 }
 output "express_route_ports_peering_location" {
-  description = "List of peering_location values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.peering_location]
+  description = "Map of peering_location values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.peering_location }
 }
 output "express_route_ports_resource_group_name" {
-  description = "List of resource_group_name values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.resource_group_name]
+  description = "Map of resource_group_name values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.resource_group_name }
 }
 output "express_route_ports_tags" {
-  description = "List of tags values across all express_route_ports"
-  value       = [for k, v in azurerm_express_route_port.express_route_ports : v.tags]
+  description = "Map of tags values across all express_route_ports, keyed the same as var.express_route_ports"
+  value       = { for k, v in azurerm_express_route_port.express_route_ports : k => v.tags }
 }
 
